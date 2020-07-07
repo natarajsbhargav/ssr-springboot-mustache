5d4d105dcc842f1f5ec4ca1a6514abc59890daba
Nataraj Srikantaiah <nsrikantaiah@Natarajs-MacBook-Pro.local>
2020-07-07 08:58:59 +0530

    Working Template

--- src/main/java/com/bhargav/ssr/model/SeoContent.java
+++ src/main/java/com/bhargav/ssr/model/SeoContent.java
@@ -3,10 +3,12 @@ package com.bhargav.ssr.model;
 public class SeoContent {
 
     private String title;
+    private String subTitle;
     private String content;
 
-    public SeoContent(String title, String content) {
+    public SeoContent(String title, String subTitle, String content) {
         this.title = title;
+        this.subTitle = subTitle;
         this.content = content;
     }
 
@@ -16,6 +18,14 @@ public class SeoContent {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public String getSubTitle() {
+        return subTitle;
+    }
+
+    public void setSubTitle(String subTitle) {
+        this.subTitle = subTitle;
     }
 
     public String getContent() {
@@ -30,8 +40,10 @@ public class SeoContent {
     public String toString() {
         final StringBuffer sb = new StringBuffer("SeoContent{");
         sb.append("title='").append(title).append('\'');
+        sb.append(", subTitle='").append(subTitle).append('\'');
         sb.append(", content='").append(content).append('\'');
         sb.append('}');
         return sb.toString();
     }
+
 }
